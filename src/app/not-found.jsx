d5634f0ef4f8167695a33840df82dc71804c6e0d
Gonodd <oddeni.gonzalo@gmail.com>
2024-05-15 15:27:02 -0300

    Correccion de margen de NotFound

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NoEncontrado (){
     return (
-        <div>
+        <div className="mt-10">
             <h1>Pagina no encontrada.</h1>
             <p>Esta pagina no se ha podido encontrar, o se encuentra en construccion, por favor regrese a la pagina principal</p>
             <Link href="/">Volver</Link>
