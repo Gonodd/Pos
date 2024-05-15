9b0dea87258fc56cfd5bdf898f64787b5e698077
Gonodd <oddeni.gonzalo@gmail.com>
2024-05-15 15:18:13 -0300

    prueba de deply automatico

--- src/app/institucional/historia/page.jsx
+++ src/app/institucional/historia/page.jsx
@@ -1,5 +1,20 @@
 export default function Historia (){
     return(
+        <div>
+
         <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        <h1>Historia</h1>
+        </div>
     )
 }
